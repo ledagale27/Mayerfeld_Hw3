2c92053399a537d774e1306a81ede0b369152100
ledagale27 <ledagale@gmail.com>
2026-04-20 05:15:49 +0100

    Initializing the project

---
+++ hw_3_project.sql
@@ -0,0 +1,63 @@
+-- 1.  List the last names of actors (alphabetically), as well as how many actors have that last name.
+
+
+
+
+
+-- 2.  List last names of actors and the number of actors who have that last name, but only for names 
+-- that are shared by at least three actors, sort so that last name with the highest number of actors appears at the top.
+
+
+
+
+
+-- 3.  List all comedy films (regardless of other genres) by displaying the title and film year and 
+-- sort by revenue_mils so that the highest appears first.
+
+
+
+
+
+-- 4.  Display how many films there are in the database for each year. Output should only contain the
+-- year and number of films with oldest films appearing first.
+
+
+
+
+
+
+-- 5.  Show all directors who have directed more than 4 films. The output should contain their names and
+-- number of films they have directed. The output should show directors with more films at the top, and if
+-- there is a tie, sort alphabetically.
+
+
+
+
+
+-- 6.  Display the highest revenue amount for each year – output should show the film_year and revenue_mils
+-- and it should show oldest films first.
+
+
+
+
+
+-- 7.  List highest postal codes for all districts staring with either A, B, C, or D ordered by the starting letter.
+
+
+
+
+
+-- 8.  Show the ID and average money spent, rounded to 3 decimal points, from 15 top spending customers 
+-- (by average spent) that spent below 4.5 so that customers who spent more appear at the top.
+
+
+
+
+
+-- 9.  Show the total number of actors who share their last name with 3 or more other actors.
+
+
+
+
+
+-- 10. Show the total revenue by month. Hint: Research “EXTRACT”.
